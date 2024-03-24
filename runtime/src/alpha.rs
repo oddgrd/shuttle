9c9fca1f4c2ea441206a5ae3a5c76e165ee4b625
oddgrd <29732646+oddgrd@users.noreply.github.com>
2024-03-24 22:06:42 +0100

    feat: experiment with runtime as client

--- runtime/src/alpha.rs
+++ runtime/src/alpha.rs
@@ -1,29 +1,22 @@
 use std::{
-    collections::BTreeMap,
+    collections::{BTreeMap, HashMap},
     iter::FromIterator,
     net::{Ipv4Addr, SocketAddr},
     ops::DerefMut,
     str::FromStr,
     sync::Mutex,
-    time::Duration,
 };
 
 use anyhow::Context;
 use async_trait::async_trait;
 use core::future::Future;
-use shuttle_common::{extract_propagation::ExtractPropagationLayer, secrets::Secret};
-use shuttle_proto::runtime::{
-    runtime_server::{Runtime, RuntimeServer},
-    LoadRequest, LoadResponse, StartRequest, StartResponse, StopReason, StopRequest, StopResponse,
-    SubscribeStopRequest, SubscribeStopResponse,
-};
-use shuttle_service::{ResourceFactory, Service};
+use shuttle_common::secrets::Secret;
+use shuttle_proto::runtime::StopReason;
+use shuttle_service::{Error, ResourceFactory, Service};
 use tokio::sync::{
     broadcast::{self, Sender},
-    mpsc, oneshot,
+    oneshot,
 };
-use tokio_stream::wrappers::ReceiverStream;
-use tonic::{transport::Server, Request, Response, Status};
 
 use crate::args::args;
 use crate::print_version;
@@ -44,16 +37,16 @@ pub async fn start(loader: impl Loader + Send + 'static, runner: impl Runner + S
         return;
     }
 
-    let args = match Args::parse() {
-        Ok(args) => args,
-        Err(e) => {
-            eprintln!("Runtime received malformed or incorrect args, {e}");
-            let help_str = "[HINT]: Run your Shuttle app with `cargo shuttle run`";
-            let wrapper_str = "-".repeat(help_str.len());
-            eprintln!("{wrapper_str}\n{help_str}\n{wrapper_str}");
-            return;
-        }
-    };
+    // let args = match Args::parse() {
+    //     Ok(args) => args,
+    //     Err(e) => {
+    //         eprintln!("Runtime received malformed or incorrect args, {e}");
+    //         let help_str = "[HINT]: Run your Shuttle app with `cargo shuttle run`";
+    //         let wrapper_str = "-".repeat(help_str.len());
+    //         eprintln!("{wrapper_str}\n{help_str}\n{wrapper_str}");
+    //         return;
+    //     }
+    // };
 
     println!("{} {} executable started", crate::NAME, crate::VERSION);
 
@@ -83,24 +76,74 @@ pub async fn start(loader: impl Loader + Send + 'static, runner: impl Runner + S
         println!("To disable it and use your own, check the docs: https://docs.shuttle.rs/configuration/logs");
     }
 
-    // where to serve the gRPC control layer
-    let addr = SocketAddr::new(Ipv4Addr::LOCALHOST.into(), args.port);
+    // TODO: initiate the runtime client, call load, then call start with the response.
+    // We will call these consecutively on startup, rather than waiting for the runner to call them.
+    // We will send the load request to the deployer sidecar, which will inject a secret and send
+    // it to the control plane to be fulfilled.
+    // TODO: change below to only have a server for HC, client for the rest.
+    // // Address to reach the sidecar service.
+    // let addr = SocketAddr::new(Ipv4Addr::LOCALHOST.into(), 3000);
 
-    let mut server_builder = Server::builder()
-        .http2_keepalive_interval(Some(Duration::from_secs(60)))
-        .layer(ExtractPropagationLayer);
+    let client = reqwest::Client::new();
 
-    let router = {
-        let alpha = Alpha::new(loader, runner);
+    println!("calling runner server");
+    let response = client
+        .get("http://runner:3000")
+        .send()
+        .await
+        .context("failed to call control plane")
+        .unwrap();
 
-        let svc = RuntimeServer::new(alpha);
-        server_builder.add_service(svc)
-    };
+    println!("runner call status code: {}", response.status());
 
-    match router.serve(addr).await {
-        Ok(_) => {}
-        Err(e) => panic!("Error while serving address {addr}: {e}"),
-    };
+    let response_body = response
+        .text()
+        .await
+        .context("failed to read response body")
+        .unwrap();
+
+    println!("provisioning call body: {response_body}");
+
+    let alpha = Alpha::new(loader, runner);
+
+    let load = alpha
+        .load(LoadRequest {
+            secrets: Default::default(),
+            project_name: "test-proj".to_string(),
+            env: "deployment".to_string(),
+        })
+        .await
+        .unwrap();
+
+    println!("load response: {:?}", load);
+
+    let start = alpha
+        .start(StartRequest {
+            ip: "http://0.0.0.0:3002".to_string(),
+            resources: Default::default(),
+        })
+        .await
+        .unwrap();
+
+    println!("start response: {:?}", start);
+
+    // TODO: subscribe stop to keep service alive? Or can we just start it directly, not as a BG task?
+
+    // let mut server_builder = Server::builder()
+    //     .http2_keepalive_interval(Some(Duration::from_secs(60)))
+    //     .layer(ExtractPropagationLayer);
+
+    // let router = {
+    //     let alpha = Alpha::new(loader, runner);
+
+    //     let svc = RuntimeServer::new(alpha);
+    //     server_builder.add_service(svc)
+    // };
+
+    // match router.serve(addr).await {
+    //     Ok(_) => {}
+    //     Err(e) => panic!("Error while serving address {addr}: {e}"),
+    // };
 }
 
 pub struct Alpha<L, R> {
@@ -111,7 +154,36 @@ pub struct Alpha<L, R> {
     runner: Mutex<Option<R>>,
 }
 
-impl<L, R> Alpha<L, R> {
+pub struct LoadRequest {
+    secrets: HashMap<String, String>,
+    project_name: String,
+    env: String,
+}
+
+#[derive(Debug)]
+pub struct LoadResponse {
+    pub success: bool,
+    pub message: String,
+    pub resources: Vec<Vec<u8>>,
+}
+
+pub struct StartRequest {
+    pub ip: String,
+    pub resources: Vec<Vec<u8>>,
+}
+
+#[derive(Default, Debug)]
+pub struct StartResponse {
+    pub success: bool,
+    pub message: String,
+}
+
+impl<L, R, S> Alpha<L, R>
+where
+    L: Loader + Send + 'static,
+    R: Runner<Service = S> + Send + 'static,
+    S: Service + 'static,
+{
     pub fn new(loader: L, runner: R) -> Self {
         let (stopped_tx, _stopped_rx) = broadcast::channel(10);
 
@@ -122,78 +194,29 @@ impl<L, R> Alpha<L, R> {
             runner: Mutex::new(Some(runner)),
         }
     }
-}
 
-#[async_trait]
-pub trait Loader {
-    async fn load(self, factory: ResourceFactory) -> Result<Vec<Vec<u8>>, shuttle_service::Error>;
-}
-
-#[async_trait]
-impl<F, O> Loader for F
-where
-    F: FnOnce(ResourceFactory) -> O + Send,
-    O: Future<Output = Result<Vec<Vec<u8>>, shuttle_service::Error>> + Send,
-{
-    async fn load(self, factory: ResourceFactory) -> Result<Vec<Vec<u8>>, shuttle_service::Error> {
-        (self)(factory).await
-    }
-}
-
-#[async_trait]
-pub trait Runner {
-    type Service: Service;
-
-    async fn run(self, resources: Vec<Vec<u8>>) -> Result<Self::Service, shuttle_service::Error>;
-}
-
-#[async_trait]
-impl<F, O, S> Runner for F
-where
-    F: FnOnce(Vec<Vec<u8>>) -> O + Send,
-    O: Future<Output = Result<S, shuttle_service::Error>> + Send,
-    S: Service,
-{
-    type Service = S;
-
-    async fn run(self, resources: Vec<Vec<u8>>) -> Result<Self::Service, shuttle_service::Error> {
-        (self)(resources).await
-    }
-}
-
-#[async_trait]
-impl<L, R, S> Runtime for Alpha<L, R>
-where
-    L: Loader + Send + 'static,
-    R: Runner<Service = S> + Send + 'static,
-    S: Service + 'static,
-{
-    async fn load(&self, request: Request<LoadRequest>) -> Result<Response<LoadResponse>, Status> {
-        let LoadRequest {
-            secrets,
-            project_name,
-            env,
-            ..
-        } = request.into_inner();
-
+    // TODO: handle errors?
+    async fn load(&self, req: LoadRequest) -> Result<LoadResponse, Error> {
         // Sorts secrets by key
-        let secrets = BTreeMap::from_iter(secrets.into_iter().map(|(k, v)| (k, Secret::new(v))));
+        let secrets =
+            BTreeMap::from_iter(req.secrets.into_iter().map(|(k, v)| (k, Secret::new(v))));
 
-        let factory = ResourceFactory::new(project_name, secrets, env.parse().unwrap());
+        let factory = ResourceFactory::new(req.project_name, secrets, req.env.parse().unwrap());
 
         let loader = self.loader.lock().unwrap().deref_mut().take().unwrap();
 
+        // TODO: run directly and panic main thread if it fails?
         // send to new thread to catch panics
-        let v = match tokio::spawn(loader.load(factory)).await {
+        let resources = match tokio::spawn(loader.load(factory)).await {
             Ok(res) => match res {
-                Ok(v) => v,
+                Ok(resources) => resources,
                 Err(error) => {
                     println!("loading service failed: {error:#}");
-                    return Ok(Response::new(LoadResponse {
+                    return Ok(LoadResponse {
                         success: false,
                         message: error.to_string(),
                         resources: vec![],
-                    }));
+                    });
                 }
             },
             Err(error) => {
@@ -207,42 +230,38 @@ where
                         },
                     };
                     println!("loading service panicked: {msg}");
-                    return Ok(Response::new(LoadResponse {
+                    return Ok(LoadResponse {
                         success: false,
                         message: msg,
                         resources: vec![],
-                    }));
+                    });
                 } else {
                     println!("loading service crashed: {error:#}");
-                    return Ok(Response::new(LoadResponse {
+                    return Ok(LoadResponse {
                         success: false,
                         message: error.to_string(),
                         resources: vec![],
-                    }));
+                    });
                 }
             }
         };
 
-        Ok(Response::new(LoadResponse {
+        Ok(LoadResponse {
             success: true,
             message: String::new(),
-            resources: v,
-        }))
+            resources,
+        })
     }
 
-    async fn start(
-        &self,
-        request: Request<StartRequest>,
-    ) -> Result<Response<StartResponse>, Status> {
-        let StartRequest { ip, resources } = request.into_inner();
-        let service_address = SocketAddr::from_str(&ip)
-            .context("invalid socket address")
-            .map_err(|err| Status::invalid_argument(err.to_string()))?;
+    async fn start(&self, request: StartRequest) -> Result<StartResponse, Error> {
+        let StartRequest { ip, resources } = request;
+        let service_address = SocketAddr::from_str(&ip).context("invalid socket address")?;
 
         let runner = self.runner.lock().unwrap().deref_mut().take().unwrap();
 
         let stopped_tx = self.stopped_tx.clone();
 
+        // TODO: run directly and panic main thread if it fails?
         // send to new thread to catch panics
         let service = match tokio::spawn(runner.run(resources)).await {
             Ok(res) => match res {
@@ -252,10 +271,10 @@ where
                     let _ = stopped_tx
                         .send((StopReason::Crash, error.to_string()))
                         .map_err(|e| println!("{e}"));
-                    return Ok(Response::new(StartResponse {
+                    return Ok(StartResponse {
                         success: false,
                         message: error.to_string(),
-                    }));
+                    });
                 }
             },
             Err(error) => {
@@ -273,22 +292,21 @@ where
                     let _ = stopped_tx
                         .send((StopReason::Crash, msg.to_string()))
                         .map_err(|e| println!("{e}"));
-                    return Ok(Response::new(StartResponse {
+                    return Ok(StartResponse {
                         success: false,
                         message: msg,
-                    }));
+                    });
                 }
                 println!("loading service crashed: {error:#}");
                 let _ = stopped_tx
                     .send((StopReason::Crash, error.to_string()))
                     .map_err(|e| println!("{e}"));
-                return Ok(Response::new(StartResponse {
+                return Ok(StartResponse {
                     success: false,
                     message: error.to_string(),
-                }));
+                });
             }
         };
-
         println!("Starting on {service_address}");
 
         let (kill_tx, kill_rx) = tokio::sync::oneshot::channel();
@@ -350,52 +368,46 @@ where
             }
         });
 
-        let message = StartResponse {
+        Ok(StartResponse {
             success: true,
             ..Default::default()
-        };
-
-        Ok(Response::new(message))
+        })
     }
+}
 
-    async fn stop(&self, _request: Request<StopRequest>) -> Result<Response<StopResponse>, Status> {
-        let kill_tx = self.kill_tx.lock().unwrap().deref_mut().take();
+#[async_trait]
+pub trait Loader {
+    async fn load(self, factory: ResourceFactory) -> Result<Vec<Vec<u8>>, shuttle_service::Error>;
+}
 
-        if let Some(kill_tx) = kill_tx {
-            if kill_tx.send("stopping deployment".to_owned()).is_err() {
-                println!("the kill receiver dropped");
-                return Err(Status::internal("failed to stop deployment"));
-            }
-
-            Ok(Response::new(StopResponse { success: true }))
-        } else {
-            println!("failed to stop deployment");
-
-            Ok(tonic::Response::new(StopResponse { success: false }))
-        }
+#[async_trait]
+impl<F, O> Loader for F
+where
+    F: FnOnce(ResourceFactory) -> O + Send,
+    O: Future<Output = Result<Vec<Vec<u8>>, shuttle_service::Error>> + Send,
+{
+    async fn load(self, factory: ResourceFactory) -> Result<Vec<Vec<u8>>, shuttle_service::Error> {
+        (self)(factory).await
     }
+}
 
-    type SubscribeStopStream = ReceiverStream<Result<SubscribeStopResponse, Status>>;
+#[async_trait]
+pub trait Runner {
+    type Service: Service;
 
-    async fn subscribe_stop(
-        &self,
-        _request: Request<SubscribeStopRequest>,
-    ) -> Result<Response<Self::SubscribeStopStream>, Status> {
-        let mut stopped_rx = self.stopped_tx.subscribe();
-        let (tx, rx) = mpsc::channel(1);
+    async fn run(self, resources: Vec<Vec<u8>>) -> Result<Self::Service, shuttle_service::Error>;
+}
 
-        // Move the stop channel into a stream to be returned
-        tokio::spawn(async move {
-            while let Ok((reason, message)) = stopped_rx.recv().await {
-                tx.send(Ok(SubscribeStopResponse {
-                    reason: reason as i32,
-                    message,
-                }))
-                .await
-                .unwrap();
-            }
-        });
+#[async_trait]
+impl<F, O, S> Runner for F
+where
+    F: FnOnce(Vec<Vec<u8>>) -> O + Send,
+    O: Future<Output = Result<S, shuttle_service::Error>> + Send,
+    S: Service,
+{
+    type Service = S;
 
-        Ok(Response::new(ReceiverStream::new(rx)))
+    async fn run(self, resources: Vec<Vec<u8>>) -> Result<Self::Service, shuttle_service::Error> {
+        (self)(resources).await
     }
 }
